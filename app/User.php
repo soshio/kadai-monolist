665b98620fbc734f0a56864de959115fa7839afb
S.S <tectest58@gmail.com>
2018-07-05 09:29:29 +0000

    add have

--- app/User.php
+++ app/User.php
@@ -39,7 +39,14 @@ class User extends Authenticatable
     {
         return $this->items()->where('type', 'want');
     }
+    
+     public function have_items()
+    {
+        return $this->items()->where('type', 'have');
+    }
 
+
+    //wantメソッド
     public function want($itemId)
     {
         // 既に Want しているかの確認
@@ -55,6 +62,7 @@ class User extends Authenticatable
         }
     }
 
+    //wantを外すメソッド
     public function dont_want($itemId)
     {
         // 既に Want しているかの確認
@@ -69,7 +77,41 @@ class User extends Authenticatable
             return false;
         }
     }
+    
+     //haveメソッド
+    public function have($itemId)
+    {
+        // 既に have しているかの確認
+        $exist = $this->is_having($itemId);
 
+        if ($exist) {
+            // 既に have していれば何もしない
+            return false;
+        } else {
+            // 未 have であれば have する
+            $this->items()->attach($itemId, ['type' => 'have']);
+            return true;
+        }
+    }
+
+    //haveを外すメソッド
+    public function dont_have($itemId)
+    {
+        // 既に have しているかの確認
+        $exist = $this->is_wanting($itemId);
+
+        if ($exist) {
+            // 既に have していれば have を外す
+            \DB::delete("DELETE FROM item_user WHERE user_id = ? AND item_id = ? AND type = 'have'", [\Auth::id(), $itemId]);
+            return true;
+        } else {
+            // 未 Want であれば何もしない
+            return false;
+        }
+    }
+    
+    
+    //既にwantしているか確認のためのメソッド
     public function is_wanting($itemIdOrCode)
     {
         if (is_numeric($itemIdOrCode)) {
@@ -77,6 +119,18 @@ class User extends Authenticatable
             return $item_id_exists;
         } else {
             $item_code_exists = $this->want_items()->where('code', $itemIdOrCode)->exists();
+            return $item_code_exists;
+        }
+    }
+    
+    //既にhaveしているか確認のためのメソッド
+     public function is_having($itemIdOrCode)
+    {
+        if (is_numeric($itemIdOrCode)) {
+            $item_id_exists = $this->have_items()->where('item_id', $itemIdOrCode)->exists();
+            return $item_id_exists;
+        } else {
+            $item_code_exists = $this->have_items()->where('code', $itemIdOrCode)->exists();
             return $item_code_exists;
         }
     }
